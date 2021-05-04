f6f1366b3581f9a5bde932985dfdb6ed142ea1a1
Byenkya <byenkyajoshua1997@gmail.com>
2021-05-04 09:30:00 +0300

    more changes to the sending email functionality

--- Application/__init__.py
+++ Application/__init__.py
@@ -107,11 +107,9 @@ api.add_resource(ForgotPasswordResource, '/api/v1/forgot_password')
 
 #Register Blueprints
 from Application.routes.customer_care.routes import customer_care
-from Application.routes.password_reset.password_reset import password_reset
 from Application.flask_imports import _session, request
 
 app.register_blueprint(customer_care)
-app.register_blueprint(password_reset)
 
 @app.after_request
 def after_request_function(response):
@@ -151,3 +149,67 @@ def before_request_function():
 @app.teardown_request
 def remove_session(exception=None):
     session.remove()
+
+from Application.flask_imports import *
+from Application.helpers.generators import TokenGenerator
+from Application.utils.email import reset_email
+from Application.database.models import Customer, Resturant, StaffAccounts
+from Application.routes.password_reset.forms import ForgotPasswordForm, NewPasswordForm
+
+@app.route("/forgot_password/<string:user_type>", methods=["GET", "POST"])
+def forgot_password(user_type):
+    form = ForgotPasswordForm()
+    if form.validate_on_submit():
+        if user_type == "customer":
+            user = Customer.read_customer(email=form.email.data.strip())
+        elif user_type == "clickeat_employee":
+            user = StaffAccounts.read_user(email=form.email.data)
+        else:
+            flash("Failed to send password reset.", "danger")
+            return redirect(url_for(".forgot_password", user_type=user_type))
+
+        if user is not None:
+            try:
+                token_gen = TokenGenerator(user=user)
+                token = token_gen.generate_password_reset_token()
+                mail_ = reset_email
+                mail_.context = dict(
+                    user_name=user.name,
+                    token=token
+                )
+                mail_.recipients = [user.email]
+                mail_.send()  
+                flash("If you provided a right email, check your email inbox for password reset link.", "success")
+                return redirect(url_for(".forgot_password", user_type=user_type))
+
+            except Exception as e:
+                print("KKKKKKKKK", e)
+
+       
+
+    context = dict(form=form, user_type=user_type)
+    return render_template("reset.html", **context)
+
+@app.route("/new_password/<string:token>", methods=["GET", "POST"])
+def set_new_password(token):
+    form = NewPasswordForm()
+    if form.validate_on_submit():
+        token_gen = TokenGenerator()
+        token_gen.verify_password_token(token)
+        user = token_gen.user
+        if user != None:
+            user.password = form.new_password.data
+            session.commit()
+            mail_  = general_email
+            mail_.context = dict(
+                user_name=user.name,
+                text = "Your password has been reset successfully."
+            )
+            mail_.title= "Password Reset"
+            mail_.recipients = [user.email]
+            mail_.send()
+            flash("Your new password has been reset. Please try to log in with the new password.", "success")
+        else:
+            flash("Please request a new password reset. Either this link is invalid or expired.", "danger")
+    context = dict(form=form,token=token)
+    return render_template("new_password.html",**context)
