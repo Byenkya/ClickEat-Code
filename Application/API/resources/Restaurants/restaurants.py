a4ef8c02ff5c41aac0645f7aa2d68bce94388d60
Byenkya <byenkyajoshua1997@gmail.com>
2021-11-05 09:39:06 +0300

    rest approved

--- Application/API/resources/Restaurants/restaurants.py
+++ Application/API/resources/Restaurants/restaurants.py
@@ -13,7 +13,7 @@ def rest_generator(data_list):
     _date = datetime.now(ni_timezone)
     current_time = _date.astimezone(timezone)
     for restaurant in data_list:
-        if restaurant.approved:
+        if restaurant.approved != False:
             _start_date = ni_timezone.localize(restaurant.operation_start_time)
             _end_date = ni_timezone.localize(restaurant.operation_stop_time)
             operation_start_time = _start_date.astimezone(timezone)
@@ -26,23 +26,5 @@ def rest_generator(data_list):
                 yield restaurant.serialize()
 
 class RestaurantApi(Resource):
-    def get(self):
-        # _date = datetime.now(ni_timezone)
-        # current_time = _date.astimezone(timezone)
-        # rests = Resturant.read_restaurants()
-        # operational_restaurants = []
-        # for restaurant in rests:
-        #     if restaurant.approved:
-        #         _start_date = ni_timezone.localize(restaurant.operation_start_time)
-        #         _end_date = ni_timezone.localize(restaurant.operation_stop_time)
-        #         operation_start_time = _start_date.astimezone(timezone)
-        #         operation_stop_time = _end_date.astimezone(timezone)
-        #         if current_time.hour >= operation_start_time.hour and current_time.hour <= operation_stop_time.hour:
-        #             rest = restaurant.serialize()
-        #             rest["operational_status"] = True
-        #             operational_restaurants.append(rest)
-        #         else:
-        #             rest = restaurant.serialize()
-        #             operational_restaurants.append(rest)    
-                    
+    def get(self):             
         return jsonify(restaurants = list(rest_generator(Resturant.read_restaurants())))
